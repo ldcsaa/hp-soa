a1b8aa2e191092ba4f82a2b8df6f195445c83716
liangdc <ldcsaa@21cn.com>
2023-10-12 17:59:41 +0800

    20231012

--- misc/opt/deploy/scripts/env.sh
+++ misc/opt/deploy/scripts/env.sh
@@ -2,8 +2,7 @@
 
 BIN_DIR=$(cd $(dirname $0); pwd)
 DEPLOY_DIR=$BIN_DIR/../deploy
-export PRG_HOME=$(cd $(dirname $BIN_DIR); pwd)
-export CONF_FILE="$DEPLOY_DIR/bootstrap.yml"
+CONF_FILE="$DEPLOY_DIR/bootstrap.yml"
 
 if [ ! -f "$CONF_FILE" ]; then
     echo "  > environment check failed -> (config file '$CONF_FILE' not exists)"
@@ -12,14 +11,14 @@ fi
 
 source /etc/profile
 
-export DEFAULT_JVM_OPTIONS=${DEFAULT_JVM_OPTIONS:--Xms256m -Xmx256m -Xss256k -XX:MaxDirectMemorySize=128m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m -XX:ReservedCodeCacheSize=256m}
-export DEFAULT_STARTUP_WAIT_SECONDS=${DEFAULT_STARTUP_WAIT_SECONDS:-90}
+DEFAULT_JVM_MEMORY_OPTIONS=${DEFAULT_JVM_MEMORY_OPTIONS:--Xms256m -Xmx256m -Xss256k -XX:MaxDirectMemorySize=128m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m -XX:ReservedCodeCacheSize=256m}
+DEFAULT_STARTUP_WAIT_SECONDS=${DEFAULT_STARTUP_WAIT_SECONDS:-90}
 
-export RUNTIME_ENV=${RUNTIME_ENV:-local}
-export SYSTEM_PROPERTIES_FILE=${SYSTEM_PROPERTIES_FILE:-/opt/hp-soa/config/system-config.properties}
-export EXTENDED_PROPERTIES_FILE=${EXTENDED_PROPERTIES_FILE:-/opt/hp-soa/config/extended-config.properties}
-export DUBBO_RESOLVE_FILE=${DUBBO_RESOLVE_FILE:-/opt/hp-soa/config/dubbo-resolve.properties}
-export JAVA_AGENT_FILE=${JAVA_AGENT_FILE:-/opt/hp-soa/config/java-agent.config}
+RUNTIME_ENV=${RUNTIME_ENV:-local}
+SYSTEM_PROPERTIES_FILE=${SYSTEM_PROPERTIES_FILE:-/opt/hp-soa/config/system-config.properties}
+EXTENDED_PROPERTIES_FILE=${EXTENDED_PROPERTIES_FILE:-/opt/hp-soa/config/extended-config.properties}
+DUBBO_RESOLVE_FILE=${DUBBO_RESOLVE_FILE:-/opt/hp-soa/config/dubbo-resolve.properties}
+JAVA_AGENT_FILE=${JAVA_AGENT_FILE:-/opt/hp-soa/config/java-agent.config}
 
 APP_NAME_KEY='hp.soa.web.app.name'
 SERVER_PORT_KEY='server.port'
@@ -56,18 +55,9 @@ if [[ -z "$APP_NAME" || -z "$SERVER_PORT"  ]]; then
     exit 1
 fi
 
-if [[ -z "$JVM_OPTIONS" || "${JVM_OPTIONS,,}" == "default" ]]; then
-    JVM_OPTIONS="$DEFAULT_JVM_OPTIONS"
-fi
-
 if [[ -z "$STARTUP_WAIT_SECONDS" || "$STARTUP_WAIT_SECONDS" -le 0 ]]; then
     STARTUP_WAIT_SECONDS=$DEFAULT_STARTUP_WAIT_SECONDS
 fi
-
-export APP_NAME
-export SERVER_PORT
-export JVM_OPTIONS
-export STARTUP_WAIT_SECONDS
 
 if [ -z "$LOG_LEVEL"]; then
     if [ "$RUNTIME_ENV" == "prod" ]; then
@@ -77,9 +67,8 @@ if [ -z "$LOG_LEVEL"]; then
     fi
 fi
 
-export LOG_LEVEL
-export LOG_FILE_PATH=${LOG_FILE_PATH:-/data/logs/access}
-export LOG_FILE="${LOG_FILE_PATH}/${APP_NAME}/service.log"
+LOG_FILE_PATH=${LOG_FILE_PATH:-/data/logs/access}
+LOG_FILE="${LOG_FILE_PATH}/${APP_NAME}/service.log"
 
 if [ -z "$JAVA_AGENT"]; then
     if [ -f "$JAVA_AGENT_FILE" ]; then
@@ -99,4 +88,66 @@ if [ -z "$JAVA_AGENT"]; then
     fi
 fi
 
-export JAVA_AGENT
+JAVA_OPTS="\
+-DskipTests=true \
+-Dfile.encoding=UTF-8 \
+-Djava.awt.headless=true \
+-Djava.security.egd=file:/dev/./urandom \
+-Dnetworkaddress.cache.ttl=10 -Dnetworkaddress.cache.negative.ttl=1 \
+$DEFAULT_JVM_MEMORY_OPTIONS \
+-XX:-OmitStackTraceInFastThrow -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=50 -XX:+HeapDumpOnOutOfMemoryError \
+-XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions -XX:GuaranteedSafepointInterval=0 -XX:+UseCountedLoopSafepoints -XX:LoopStripMiningIter=1000 \
+--add-opens java.base/java.lang=ALL-UNNAMED \
+--add-opens java.base/java.lang.reflect=ALL-UNNAMED \
+--add-opens java.base/java.util=ALL-UNNAMED \
+--add-opens java.base/java.util.concurrent=ALL-UNNAMED \
+--add-opens java.base/java.util.concurrent.atomic=ALL-UNNAMED \
+--add-opens java.base/java.io=ALL-UNNAMED \
+--add-opens java.base/java.nio=ALL-UNNAMED \
+--add-opens java.base/java.math=ALL-UNNAMED \
+--add-opens java.base/java.text=ALL-UNNAMED \
+--add-opens java.base/java.time=ALL-UNNAMED \
+--add-opens java.base/java.net=ALL-UNNAMED \
+--add-opens java.base/javax.net.ssl=ALL-UNNAMED \
+--add-opens java.base/java.security=ALL-UNNAMED \
+--add-opens java.rmi/sun.rmi.transport=ALL-UNNAMED \
+--add-opens java.base/jdk.internal.access=ALL-UNNAMED \
+--add-opens java.base/jdk.internal.misc=ALL-UNNAMED"
+
+if [ -n "$LOG_LEVEL" ]; then
+    JAVA_OPTS="-Dlog4j.log.level=$LOG_LEVEL $JAVA_OPTS"
+fi
+
+if [ -n "$LOG_FILE_PATH" ]; then
+    JAVA_OPTS="-Dlog4j.logfile.path=$LOG_FILE_PATH $JAVA_OPTS"
+fi
+
+if [ -n "$DUBBO_RESOLVE_FILE" ]; then
+    JAVA_OPTS="-Ddubbo.resolve.file=$DUBBO_RESOLVE_FILE $JAVA_OPTS"
+fi
+
+if [ -n "$EXTENDED_PROPERTIES_FILE" ]; then
+    JAVA_OPTS="-Dhp.soa.extended.properties.file=$EXTENDED_PROPERTIES_FILE $JAVA_OPTS"
+fi
+
+if [ -n "$SYSTEM_PROPERTIES_FILE" ]; then
+    JAVA_OPTS="-Dhp.soa.system.properties.file=$SYSTEM_PROPERTIES_FILE $JAVA_OPTS"
+fi
+
+if [ -n "$RUNTIME_ENV" ]; then
+    JAVA_OPTS="-Dspring.profiles.active=$RUNTIME_ENV $JAVA_OPTS"
+fi
+
+if [ -n "$JVM_OPTIONS" ]; then
+    JAVA_OPTS="$JAVA_OPTS $JVM_OPTIONS"
+fi
+
+if [ -n "$JAVA_AGENT" ]; then
+    JAVA_OPTS="$JAVA_OPTS $JAVA_AGENT"
+fi
+
+export APP_NAME
+export SERVER_PORT
+export STARTUP_WAIT_SECONDS
+export LOG_FILE
+export JAVA_OPTS
--- misc/opt/deploy/scripts/start.sh
+++ misc/opt/deploy/scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BIN_DIR=$(cd $(dirname $0); pwd)
+PRG_HOME=$(cd $(dirname $BIN_DIR); pwd)
 
 source $BIN_DIR/env.sh
 
@@ -20,63 +21,6 @@ done
 if [[ -z "$PRG_JAR" || ! -f "$PRG_JAR" ]]; then
     echo "  > start failed -> (main jar file '$PRG_JAR' not exists)"
     exit 2
-fi
-
-JAVA_OPTS="\
--DskipTests=true \
--Dfile.encoding=UTF-8 \
--Djava.awt.headless=true \
--Djava.security.egd=file:/dev/./urandom \
--Dnetworkaddress.cache.ttl=10 -Dnetworkaddress.cache.negative.ttl=1 \
--XX:-OmitStackTraceInFastThrow -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=50 -XX:+HeapDumpOnOutOfMemoryError \
--XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions -XX:GuaranteedSafepointInterval=0 -XX:+UseCountedLoopSafepoints -XX:LoopStripMiningIter=1000 \
---add-opens java.base/java.lang=ALL-UNNAMED \
---add-opens java.base/java.lang.reflect=ALL-UNNAMED \
---add-opens java.base/java.util=ALL-UNNAMED \
---add-opens java.base/java.util.concurrent=ALL-UNNAMED \
---add-opens java.base/java.util.concurrent.atomic=ALL-UNNAMED \
---add-opens java.base/java.io=ALL-UNNAMED \
---add-opens java.base/java.nio=ALL-UNNAMED \
---add-opens java.base/java.math=ALL-UNNAMED \
---add-opens java.base/java.text=ALL-UNNAMED \
---add-opens java.base/java.time=ALL-UNNAMED \
---add-opens java.base/java.net=ALL-UNNAMED \
---add-opens java.base/javax.net.ssl=ALL-UNNAMED \
---add-opens java.base/java.security=ALL-UNNAMED \
---add-opens java.rmi/sun.rmi.transport=ALL-UNNAMED \
---add-opens java.base/jdk.internal.access=ALL-UNNAMED \
---add-opens java.base/jdk.internal.misc=ALL-UNNAMED"
-
-if [ -n "$DUBBO_RESOLVE_FILE" ]; then
-    JAVA_OPTS="-Ddubbo.resolve.file=$DUBBO_RESOLVE_FILE $JAVA_OPTS"
-fi
-
-if [ -n "$EXTENDED_PROPERTIES_FILE" ]; then
-    JAVA_OPTS="-Dhp.soa.extended.properties.file=$EXTENDED_PROPERTIES_FILE $JAVA_OPTS"
-fi
-
-if [ -n "$SYSTEM_PROPERTIES_FILE" ]; then
-    JAVA_OPTS="-Dhp.soa.system.properties.file=$SYSTEM_PROPERTIES_FILE $JAVA_OPTS"
-fi
-
-if [ -n "$LOG_FILE_PATH" ]; then
-    JAVA_OPTS="-Dlog4j.logfile.path=$LOG_FILE_PATH $JAVA_OPTS"
-fi
-
-if [ -n "$LOG_LEVEL" ]; then
-    JAVA_OPTS="-Dlog4j.log.level=$LOG_LEVEL $JAVA_OPTS"
-fi
-
-if [ -n "$RUNTIME_ENV" ]; then
-    JAVA_OPTS="-Dspring.profiles.active=$RUNTIME_ENV $JAVA_OPTS"
-fi
-
-if [ -n "$JVM_OPTIONS" ]; then
-    JAVA_OPTS="$JVM_OPTIONS $JAVA_OPTS"
-fi
-
-if [ -n "$JAVA_AGENT" ]; then
-    JAVA_OPTS="$JAVA_OPTS $JAVA_AGENT"
 fi
 
 check_port()
