108d20a074faa82e3be5fb8c81bab6c3911cd191
liangdc <ldcsaa@21cn.com>
2023-10-12 14:23:57 +0800

    20231012

--- misc/opt/deploy/scripts/env.sh
+++ misc/opt/deploy/scripts/env.sh
@@ -12,16 +12,19 @@ fi
 
 source /etc/profile
 
-RUNTIME_ENV=${environment:-local}
+export DEFAULT_JVM_OPTIONS=${DEFAULT_JVM_OPTIONS:--Xms256m -Xmx256m -Xss256k -XX:MaxDirectMemorySize=128m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m -XX:ReservedCodeCacheSize=256m}
+export DEFAULT_STARTUP_WAIT_SECONDS=${DEFAULT_STARTUP_WAIT_SECONDS:-90}
+
+export RUNTIME_ENV=${RUNTIME_ENV:-local}
+export SYSTEM_PROPERTIES_FILE=${SYSTEM_PROPERTIES_FILE:-/opt/hp-soa/config/system-config.properties}
+export EXTENDED_PROPERTIES_FILE=${EXTENDED_PROPERTIES_FILE:-/opt/hp-soa/config/extended-config.properties}
+export DUBBO_RESOLVE_FILE=${DUBBO_RESOLVE_FILE:-/opt/hp-soa/config/dubbo-resolve.properties}
+export JAVA_AGENT_FILE=${JAVA_AGENT_FILE:-/opt/hp-soa/config/java-agent.config}
+
 APP_NAME_KEY='hp.soa.web.app.name'
 SERVER_PORT_KEY='server.port'
 JVM_OPTIONS_KEY="jvm.options.${RUNTIME_ENV}"
 STARTUP_WAIT_SECONDS_KEY="startup.max-wait-seconds"
-
-APP_NAME=
-SERVER_PORT=
-JVM_OPTIONS=
-STARTUP_WAIT_SECONDS=
 
 OLD_IFS="$IFS"
 IFS="#"
@@ -54,51 +57,46 @@ if [[ -z "$APP_NAME" || -z "$SERVER_PORT"  ]]; then
 fi
 
 if [[ -z "$JVM_OPTIONS" ]]; then
-    JVM_OPTIONS="-Xms256m -Xmx256m -Xss256k -XX:MaxDirectMemorySize=128m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m -XX:ReservedCodeCacheSize=256m"
+    JVM_OPTIONS="$DEFAULT_JVM_OPTIONS"
 fi
 
 if [[ -z "$STARTUP_WAIT_SECONDS" || "$STARTUP_WAIT_SECONDS" -le 0 ]]; then
-    STARTUP_WAIT_SECONDS=90
+    STARTUP_WAIT_SECONDS=$DEFAULT_STARTUP_WAIT_SECONDS
 fi
 
-export RUNTIME_ENV
 export APP_NAME
 export SERVER_PORT
 export JVM_OPTIONS
 export STARTUP_WAIT_SECONDS
 
-LOG_LEVEL=
-
-if [ "$RUNTIME_ENV" == "prod" ]; then
-    LOG_LEVEL="INFO"
-else
-    LOG_LEVEL="DEBUG"
+if [ -z "$LOG_LEVEL"]; then
+    if [ "$RUNTIME_ENV" == "prod" ]; then
+        LOG_LEVEL="INFO"
+    else
+        LOG_LEVEL="DEBUG"
+    fi
 fi
 
 export LOG_LEVEL
-export LOG_FILE_PATH="/data/logs/access"
+export LOG_FILE_PATH=${LOG_FILE_PATH:-/data/logs/access}
 export LOG_FILE="${LOG_FILE_PATH}/${APP_NAME}/service.log"
-export SYSTEM_PROPERTIES_FILE="/opt/hp-soa/config/system-config.properties"
-export EXTENDED_PROPERTIES_FILE="/opt/hp-soa/config/extended-config.properties"
-export DUBBO_RESOLVE_FILE="/opt/hp-soa/config/dubbo-resolve.properties"
-export JAVA_AGENT_FILE="/opt/hp-soa/config/java-agent.config"
 
-JAVA_AGENT=
-
-if [ -f "$JAVA_AGENT_FILE" ]; then
-    while read LINE
-    do
-        if [[ -n "$LINE" && ${LINE:0:1} != "#" ]]; then
-            LINE=${LINE//\$\{APP_NAME\}/$APP_NAME}
-            LINE=${LINE//\$APP_NAME/$APP_NAME}
-            
-            if [ -z "$JAVA_AGENT" ]; then
-                JAVA_AGENT="$LINE"
-            else
-                JAVA_AGENT="$JAVA_AGENT $LINE"
+if [ -z "$JAVA_AGENT"]; then
+    if [ -f "$JAVA_AGENT_FILE" ]; then
+        while read LINE
+        do
+            if [[ -n "$LINE" && ${LINE:0:1} != "#" ]]; then
+                LINE=${LINE//\$\{APP_NAME\}/$APP_NAME}
+                LINE=${LINE//\$APP_NAME/$APP_NAME}
+                
+                if [ -z "$JAVA_AGENT" ]; then
+                    JAVA_AGENT="$LINE"
+                else
+                    JAVA_AGENT="$JAVA_AGENT $LINE"
+                fi
             fi
-        fi
-    done < $JAVA_AGENT_FILE
+        done < $JAVA_AGENT_FILE
+    fi
 fi
 
 export JAVA_AGENT
